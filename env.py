936e92d76fceca899fcc93df0387402c0b725489
Cheryl <106958690+ccp84@users.noreply.github.com>
2023-04-28 14:19:37 +0100

    Delete env.py

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ["AWS_ACCESS_KEY_ID"] = 'AKIARCVLNTFWMFXCM3M5'
-os.environ["AWS_SECRET_KEY"] = '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'
-os.environ["DATABASE_URL"] = 'postgres://eudktewh:ulvk6esZ5Nr6UVd7IcpgKAPpnqYmzyfE@manny.db.elephantsql.com/eudktewh'
-os.environ["SECRET_KEY"] = 'DRYRUNSECRETKEY'
