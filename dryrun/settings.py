1ee987be8c645cf40f2489ecdb078eb19702114e
ccp84 <ccp84@icloud.com>
2023-04-28 13:09:21 +0000

    tools>update allowed hosts

--- dryrun/settings.py
+++ dryrun/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dryrun-api.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['dryrun-api.herokuapp.com', 'localhost']
 
 
 # Application definition
