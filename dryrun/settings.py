2ebeefb31425ddc6eef8070cba4fe1ae66f92aae
ccp84 <ccp84@icloud.com>
2023-04-28 21:02:59 +0000

    py>update allowed origins

--- dryrun/settings.py
+++ dryrun/settings.py
@@ -175,5 +175,6 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://dryrun-react.herokuapp.com"
+    "https://dryrun-react.herokuapp.com",
+    "https://dryrun-api.herokuapp.com"
 ]
