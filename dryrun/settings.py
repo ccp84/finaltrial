faeae38f392653d8cc16666cbe66261c4470529e
ccp84 <ccp84@icloud.com>
2023-04-28 21:15:22 +0000

    py>update settings

--- dryrun/settings.py
+++ dryrun/settings.py
@@ -49,9 +49,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -172,9 +172,10 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://dryrun-react.herokuapp.com",
-    "https://dryrun-api.herokuapp.com"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "https://dryrun-react.herokuapp.com",
+#     "https://dryrun-api.herokuapp.com"
+# ]
+CORS_ALLOW_ALL_ORIGINS: True
