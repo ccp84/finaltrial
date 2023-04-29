a28ab80ec7fd74a44f5bf3b7591af035e0158f8f
ccp84 <ccp84@icloud.com>
2023-04-29 10:06:57 +0000

    py>remove gitpod cors settings

--- dryrun/settings.py
+++ dryrun/settings.py
@@ -59,16 +59,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
-    ]
-CORS_ALLOW_CREDENTIALS = True
-
 ROOT_URLCONF = 'dryrun.urls'
 
 TEMPLATES = [
