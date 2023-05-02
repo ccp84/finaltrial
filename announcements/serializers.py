09956c8d689c0738c6c98e4167226125e82c3d47
ccp84 <ccp84@icloud.com>
2023-05-02 19:53:20 +0000

    py>add post id to serializer

--- announcements/serializers.py
+++ announcements/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Announcements
 
+
 class AnnouncementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Announcements
-        fields = ['title', 'content', 'author']
+        fields = ['id', 'title', 'content', 'author']
