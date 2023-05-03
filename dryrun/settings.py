710ea5380b8923c8fff419e4eca51ef527039beb
ccp84 <ccp84@icloud.com>
2023-05-03 14:43:14 +0000

    py>turn back on JWT authentication

--- dryrun/settings.py
+++ dryrun/settings.py
@@ -193,9 +193,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    # )
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 SIMPLE_JWT = {
